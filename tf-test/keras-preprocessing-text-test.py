ee97fc5dfad4f68ad0c5212a94bebfff8f82bb86
Finn <44287141+71xn@users.noreply.github.com>
2021-04-27 10:24:30 +0100

    updated text pre-processing

--- tf-test/keras-preprocessing-text-test.py
+++ tf-test/keras-preprocessing-text-test.py
@@ -3,13 +3,21 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+vocab_size = 1000
+embedding_dim = 32
+max_length = 16
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = "<OOV>"
+training_size = 20000
+
 sentences = [
     "Test sentence 1",
     "Test sentence 2",
     "This is another test sentence"
 ]
 
-tokenizer = Tokenizer(num_words = 100)
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
 
 tokenizer.fit_on_texts(sentences)
 
@@ -17,7 +25,10 @@ word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(sentences)
 
-padded = pad_sequences(sequences, padding='post', truncating='post', maxlen=5)
+padded = pad_sequences(sequences, padding=padding_type, truncating=trunc_type, maxlen=5)
+
+label_tokenizer = Tokenizer()
+
 
 print(word_index)
 print(sequences)
